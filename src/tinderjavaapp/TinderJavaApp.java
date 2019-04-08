bfef5ad3ef5f6936dee24a866be27d81363c01bc
alumnogreibd <alumnogreibd@alumnogreibd-VirtualBox>
2019-04-08 09:43:38 +0200

    modo diablo eskereee !!! :-)
    //Kike

--- src/tinderjavaapp/TinderJavaApp.java
+++ src/tinderjavaapp/TinderJavaApp.java
@@ -16,6 +16,7 @@ public class TinderJavaApp {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //Mientras empujas ese booty pa'tras
     }
     
 }
