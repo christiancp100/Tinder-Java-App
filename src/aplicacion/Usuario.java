03e0201634ad96d1f2a95bf3210d09f874096678
christiancp <christiancp100@gmail.com>
2019-04-10 13:32:10 +0200

    Alla va por quinta vez!

---
+++ src/aplicacion/Usuario.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacion;
+
+/**
+ *
+ * @author alumnogreibd
+ */
+public class Usuario {
+    private String nombre;
+    private String contrasenha;
+
+    public Usuario(String nombre, String contrasenha) {
+        this.nombre = nombre;
+        this.contrasenha = contrasenha;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContrasenha() {
+        return contrasenha;
+    }
+
+    public void setContrasenha(String contrasenha) {
+        this.contrasenha = contrasenha;
+    }
+    
+    
+    
+}
