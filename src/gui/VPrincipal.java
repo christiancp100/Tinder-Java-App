561fe809cdc08c75d29c3cd21c4757721627cd4b
Urielfg <49388234+Urielfg@users.noreply.github.com>
2019-04-15 18:18:00 +0200

    Add files via upload
    se añade la opcion del código al iniciar sesión pero aún no se encontró el fallo. También está diseñada la interfaz de reportes.

--- src/gui/VPrincipal.java
+++ src/gui/VPrincipal.java
@@ -27,6 +27,7 @@ public class VPrincipal extends javax.swing.JFrame {
      */
     private Usuario usuario; //Usuario autenticado
     
+    
     public VPrincipal() {
         initComponents();
         initStyle();
@@ -450,6 +451,9 @@ public class VPrincipal extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
+        this.dispose();
+        //Faltaría añadir como segundo paráetro el nombre del que se quiere reportar
+        //VReporte aux= new VReporte(this.usuario.getNombre(),);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     
